b568f3042d50ca545915f7c2e8aea3d5593980db
rakion99 <_rakion99@outlook.com>
2018-01-12 16:50:17 -0600

    since skid dont know how to update
    updated because skids dont even know how

--- IceSource/IceSource/Skid6.h
+++ IceSource/IceSource/Skid6.h
@@ -1,28 +1,28 @@
 #pragma once
 //getfield
-unsigned long getSkId = SKID(0x7307e0);
+unsigned long getSkId = SKID(0x72fe40);
 //settop
-unsigned long SkIdtop = SKID(0x7328d0);
+unsigned long SkIdtop = SKID(0x731f80);
 //pushstring
-unsigned long pushSkId = SKID(0x731b60);
+unsigned long pushSkId = SKID(0x7311f0);
 //pushvalue
-unsigned long SkIdvalue = SKID(0x731c20);
+unsigned long SkIdvalue = SKID(0x7312c0);
 //call
-unsigned long SkIds = SKID(0x730180);
+unsigned long SkIds = SKID(0x72f7c0);
 //setfield
-unsigned long setSkId = SKID(0x7324f0);
+unsigned long setSkId = SKID(0x731ba0);
 //pushnumber
-unsigned long hOwMaNySkIdS = SKID(0x731ad0);
+unsigned long hOwMaNySkIdS = SKID(0x731160);
 //contextlevel/identity
-unsigned long SKIDLEVEL = SKID(0x5a7260);
+unsigned long SKIDLEVEL = SKID(0x5a6a90);
 //ScriptContextVFTable
-unsigned long SkIdTab = SKID(0x6b898e);
+unsigned long SkIdTab = SKID(0x6b8dbe);
 //getmetatable
-unsigned long DOGGO = SKID(0x730ad0);
+unsigned long DOGGO = SKID(0x730130);
 //pcall
-unsigned long pSkId = SKID(0x7315a0);
+unsigned long pSkId = SKID(0x730c20);
 //rawrjz
-unsigned long miaujz = SKID(0x729f17);
+unsigned long miaujz = SKID(0x729777);
 
 int Skidtest;
 int SkidState;
@@ -33,5 +33,5 @@ void SKIDD() {
 	DWORD SkIdT = *(DWORD*)(SkIdTab + 0x2);
 	Skidtest = SkId::ScanforSkid((char*)&SkIdT);
 	//luastate
-	SkidState = Skidtest + skid - *(DWORD*)(Skidtest + skid);
+	SkidState = Skidtest + skid + *(DWORD*)(Skidtest + skid);
 }
